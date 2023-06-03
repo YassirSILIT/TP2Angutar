cc0426ba54e935db0065ea20e7ba62e28e80b475
Yassir SILIT <yassir.slit03@gmail.com>
2023-06-03 02:29:30 +0100

    TpAngular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './elements/pages/home/home.component';
+import { UsersComponent } from './elements/pages/users/users.component';
+import { HeaderComponent } from './elements/header/header.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "header", component:HeaderComponent},
+  {path: "home", component: HomeComponent },
+  {path: "users", component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './elements/header/header.component';
+import { MenuComponent } from './elements/menu/menu.component';
+import { HomeComponent } from './elements/pages/home/home.component';
+import { UsersComponent } from './elements/pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
